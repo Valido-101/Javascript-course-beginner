a77dfb94a326461c0ce88a1e61fa1da6761a1cc3
Valido-101 <jesuszafra22@gmail.com>
2022-12-23 18:55:08 +0100

    Distintos tipos de declaración de variables

--- javascript-course-data-types.js
+++ javascript-course-data-types.js
@@ -3,3 +3,16 @@ Data Types:
 
 undefined, null, boolean, string, symbol, number, object
 */
+
+//declaracion de variables
+
+//con var la variable existira en todo el scope
+var myName = "Beau";
+
+myName = 8;
+
+//con let la variable solo existe en el scope en el que lo hayas creado (dentro de un if, etc)
+let ourName = "freeCodeCamp";
+
+//no se puede modificar
+const pi = 3.14;
